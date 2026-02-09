4e8b590c28d0085b999b86ba865a5a4889565155
Mamitiana-Kevin <itu.andrianarisonkevin@gmail.com>
2026-02-09 19:22:21 +0100

    merge misy tsy mety

--- project-restored/api/src/models/statusModel.js
+++
@@ -1,8 +0,0 @@
-const pool = require('../config/db');
-
-const getAllStatuses = async () => {
-  const result = await pool.query('SELECT * FROM report_statuses ORDER BY level');
-  return result.rows;
-};
-
-module.exports = { getAllStatuses };
